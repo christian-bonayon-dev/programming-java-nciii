6de13e0e11b713c934c5243cdc62a025cf9d1df2
Christian Bonayon <christian.bonayon.dev@gmail.com>
2026-06-24 11:57:54 +0800

    Update PrintHelloWorld.java

--- src/main/java/core_java/fundamentals_basics/print_helloworld/PrintHelloWorld.java
+++ src/main/java/core_java/fundamentals_basics/print_helloworld/PrintHelloWorld.java
@@ -2,6 +2,6 @@ package core_java.fundamentals_basics.print_helloworld;
 
 public class PrintHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World");      // Output: Hello World
     }
 }
