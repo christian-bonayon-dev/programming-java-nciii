188aad1667af02a040f40f34dd4d66f9bc69075a
Christian Bonayon <christian.bonayon.dev@gmail.com>
2026-06-24 09:30:10 +0800

    Update ForLoop.java

--- src/main/java/core_java/fundamentals_basics/looping_statements/ForLoop.java
+++ src/main/java/core_java/fundamentals_basics/looping_statements/ForLoop.java
@@ -2,8 +2,8 @@ package core_java.fundamentals_basics.looping_statements;
 
 public class ForLoop {
     public static void main(String[] args) {
-        for(int index = 0; index < 10; index++) {
-            System.out.println("Hello World!");
+        for(int count = 1; count <= 10; count++) {
+            System.out.println(count);
         }
     }
 }
