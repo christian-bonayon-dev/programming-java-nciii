0da8645c78e4e77f7f1fcb7a7a958652c5799ace
Christian Bonayon <christian.bonayon.dev@gmail.com>
2026-06-24 09:28:17 +0800

    Add WhileLoop.java

---
+++ src/main/java/core_java/fundamentals_basics/looping_statements/WhileLoop.java
@@ -0,0 +1,13 @@
+package core_java.fundamentals_basics.looping_statements;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1;  // Initialization (* It depends on requirements)
+
+        while(count <= 10) {
+            System.out.println(count);
+
+            count++;    // Use increment operator to avoid infinite loop (* It depends on requirements)
+        }
+    }
+}
