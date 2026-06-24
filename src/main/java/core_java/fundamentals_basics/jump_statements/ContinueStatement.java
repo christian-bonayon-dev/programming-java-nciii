390d1538c7d1178f5c3ad2c7800004a635df614d
Christian Bonayon <christian.bonayon.dev@gmail.com>
2026-06-24 18:35:10 +0800

    Update BreakStatement.java and add ContinueStatement.java

--- src/main/java/core_java/fundamentals_basics/jump_statements/ContinueStatement.java
+++ src/main/java/core_java/fundamentals_basics/jump_statements/ContinueStatement.java
@@ -1,13 +1,13 @@
 package core_java.fundamentals_basics.jump_statements;
 
-public class BreakStatement {
+public class ContinueStatement {
     public static void main(String[] args) {
         for (int count = 1; count <= 10; count++) {
-            System.out.print(count + " ");      // Output: 1 2 3 4 5 6
-
             if (count == 6) {
-                break;      // <-- exit out in loop
+                continue;      // <--skip the current iteration
             }
+
+            System.out.print(count + " ");      // Output: 1 2 3 4 5 7 8 9 10
         }
     }
 }
