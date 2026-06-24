70a2cfec24e38438c6e0e0a78b061c6c14a54e40
Christian Bonayon <christian.bonayon.dev@gmail.com>
2026-06-24 09:22:41 +0800

    Add ForLoop.java

---
+++ src/main/java/core_java/fundamentals_basics/looping_statements/ForLoop.java
@@ -0,0 +1,9 @@
+package core_java.fundamentals_basics.looping_statements;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int index = 0; index < 10; index++) {
+            System.out.println("Hello World!");
+        }
+    }
+}
