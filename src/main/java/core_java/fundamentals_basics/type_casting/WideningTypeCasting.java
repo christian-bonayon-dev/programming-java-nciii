b949835c88fea957b9771e775b31e5d1032bd670
Christian Bonayon <christian.bonayon.dev@gmail.com>
2026-06-24 10:56:38 +0800

    Delete TypeCasting.java and Add two files(Narrowing and Widening) for TypeCasting topic

---
+++ src/main/java/core_java/fundamentals_basics/type_casting/WideningTypeCasting.java
@@ -0,0 +1,11 @@
+package core_java.fundamentals_basics.type_casting;
+
+// Widening Type Casting (Implicit Type Casting)
+public class WideningTypeCasting {
+    public static void main(String[] args) {
+        int num = 10;
+        double convertedNumber = num;
+
+        System.out.println(convertedNumber);    // Output: 10.0
+    }
+}
